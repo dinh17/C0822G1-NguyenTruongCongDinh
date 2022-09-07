61c09002651214fdf573f5d21b4f163b65ba54be
dinh17 <nguyentruongcongdinh@gmail.com>
2022-09-07 14:25:59 +0700

    up_bai_tap

---
+++ ss7_bien_kieudulieu_toantu/thuc_hanh/hcn.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("nhap chieu rong");
+inputHeight = prompt("nhap choeucao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Dien tich hinh chu nhat : " + area);
