2e1834099fceeec753e5ceb24c5019d7e85e0b64
dinh17 <nguyentruongcongdinh@gmail.com>
2022-09-10 15:46:50 +0700

    lam_bai_tap

---
+++ ss9_cau_truc_dieu_kien2/bai_tap/ung_dung_do_you_love_me.js
@@ -0,0 +1,10 @@
+function yesClick() {
+    alert("<3>");
+}
+
+function noHover() {
+    var x = Math.floor(Math.random() * window.innerWidth);
+    var y = Math.floor(Math.random() * window.innerHeight);
+    document.getElementById("btnNo").style.left = x + "px";
+    document.getElementById("btnNo").style.top = y + "px";
+}
